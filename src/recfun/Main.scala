584e7a27aaa931e19aa966792733ab07ebeef23c
Rodrigo Morais <morais.rm@gmail.com>
2016-01-02 10:43:59 -0200

    Creating the repo.

---
+++ src/recfun/Main.scala
@@ -0,0 +1,11 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Money is 4 to coins 1,2. The count change is: " + countChange(4, List.range(1, 2)))
+  }
+  
+  def countChange(money: Int, coins: List[Int]): Int = {
+    1
+  }
+}
