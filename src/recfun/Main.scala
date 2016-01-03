cfddf73bb2a9e1236b799e05f41b4a06eae30a5d
Rodrigo Morais <morais.rm@gmail.com>
2016-01-03 12:01:22 -0200

    Adding the solution of counting change using functional programming with Scala.

--- src/recfun/Main.scala
+++ src/recfun/Main.scala
@@ -2,10 +2,15 @@ package recfun
 
 object Main {
   def main(args: Array[String]) {
-    println("Money is 4 to coins 1,2. The count change is: " + countChange(4, List.range(1, 2)))
+    println("Money is 4 to coins 1,2. The count change is: " + countChange(4, List(1, 2)))
+    println("Money is 4 to coins 1,2,3. The count change is: " + countChange(4, List(1, 2, 3)))
   }
   
   def countChange(money: Int, coins: List[Int]): Int = {
-    1
+    if(money < 0) 0
+    else if(coins.isEmpty)
+      if(money == 0) 1 else 0
+    else
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
